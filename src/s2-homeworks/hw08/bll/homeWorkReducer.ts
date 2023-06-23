50c678df332944dc8e2e56311715837e8b1baef0
Alexandr1991Sh <an1991sh@yandex.ru>
2023-06-23 17:06:40 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,42 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            if (action.payload === 'up') {
+                return [...state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return 1
+                    } else if (a.name < b.name) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                })]
+            }
+            if (action.payload === 'down') {
+                return [...state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    else if (a.name<b.name){
+                        return 1
+                    }
+                    else {
+                        return 0
+                    }
+                })]
+            }
+         else  return state
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
     }
 }
+
