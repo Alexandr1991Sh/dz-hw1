f0d1ecd67bc1daf271807ecfe7bb457e1967816e
Alexandr1991Sh <an1991sh@yandex.ru>
2024-03-25 16:58:04 +0300

    HW9 Clock component

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,33 +7,9 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { //
     switch (action.type) {
         case 'sort': {
-            if (action.payload === 'up') {
-                return [...state.sort(function (a, b) {
-                    if (a.name > b.name) {
-                        return 1
-                    }
-                    else if (a.name < b.name) {
-                        return -1
-                    }
-                    else {
-                        return 0
-                    }
-                })]
-            }
-            if (action.payload === 'down') {
-                return [...state.sort(function (a, b) {
-                    if (a.name > b.name) {
-                        return -1
-                    }
-                    else if (a.name < b.name) {
-                        return 1
-                    }
-                    else {
-                        return 0
-                    }
-                })]
-            }
-            else return state
+            return [...state].sort((a, b) => action.payload === 'up'
+                ? a.name.localeCompare(b.name)
+                : b.name.localeCompare(a.name))
         }
         case 'check': {
             return state.filter((el) => el.age >= action.payload)
